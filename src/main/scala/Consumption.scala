8f4c7a28569930ec64e086b87ccaa21a691181a9
Alan Lewis <alan.lewis@objectaction.com>
2017-04-26 13:22:18 +0100

    Access collection through view to speed up filter.

--- src/main/scala/Consumption.scala
+++ src/main/scala/Consumption.scala
@@ -7,7 +7,7 @@ object Consumption {
 	def aggregateMonthlyConsumption(snapshots: Seq[(ZonedDateTime, Double)]): Seq[Double] = {
 		for {
 			i <- 1 to 12 
-			mnth = snapshots withFilter { case (d, v) => d.getMonthValue() == i } map { _._2 } sum
+			mnth = snapshots.view withFilter { case (d, v) => d.getMonthValue() == i } map { _._2 } sum
 		} yield mnth
 	}
 }
